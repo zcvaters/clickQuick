3fb25ccfa04adb4629ac05e0a3de3dbb213646e7
Zach Vaters <zachvaters@gmail.com>
2020-03-29 22:06:40 -0230

    Reconfigured

--- src/main.py
+++ src/main.py
@@ -1,2 +1,16 @@
+from pynput import mouse, keyboard
+from pynput.mouse import Button, Controller, Listener
+import time
 
+mousey = Controller()
+listener = Listener()
 
+def simpleClicker():
+    time.sleep(10)
+    mousey.click(Button.left, 500)
+
+do = True
+while do == True:
+    print(mousey.position)
+    if listener.on_click() is False:
+        do = False
